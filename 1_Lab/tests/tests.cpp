3ab7f496c49a5b0b34e24173b429a5b9bfb1e2b4
BaronPipistron <baronpipistron@mail.ru>
2023-09-22 09:25:01 +0300

    TESTS FIX

--- 1_Lab/tests/tests.cpp
+++ 1_Lab/tests/tests.cpp
@@ -1,32 +1,32 @@
 #include <gtest/gtest.h>
 #include "../include/task.hpp"
 
-TEST(test_01, basic_test_set)
+TEST(task_func_test, basic_test_set)
 {
     ASSERT_TRUE(MAI::OOP::Lab1::Solution::task(1) == "00000000000000000000000000000001");
 }
 
-TEST(test_02, basic_test_set)
+TEST(task_func_test, param_null_test_set)
 {
     ASSERT_TRUE(MAI::OOP::Lab1::Solution::task(0) == "00000000000000000000000000000000");
 }
 
-TEST(test_03, basic_test_set)
+TEST(task_func_test, random_param_test_set)
 {
     ASSERT_TRUE(MAI::OOP::Lab1::Solution::task(14) == "00000000000000000000000000001110");
 }
 
-TEST(test_04, basic_test_set)
+TEST(task_func_test, negative_param_test_set)
 {
     ASSERT_TRUE(MAI::OOP::Lab1::Solution::task(-14) == "10000000000000000000000000001110");
 }
 
-TEST(test_05, basic_test_set)
+TEST(task_func_test, max_32_bit_int_test_set)
 {
     ASSERT_TRUE(MAI::OOP::Lab1::Solution::task(2147483647) == "01111111111111111111111111111111");
 }
 
-TEST(test_06, basic_test_set)
+TEST(task_func_test, min_32_bit_int_test_set)
 {
     ASSERT_TRUE(MAI::OOP::Lab1::Solution::task(-2147483647) == "11111111111111111111111111111111");
 }
