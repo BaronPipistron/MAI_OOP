8a2e8793a302344a2800fc4e8b82ee916545690b
BaronPipistron <baronpipistron@mail.ru>
2023-11-16 22:03:47 +0300

    fixes and add vector tests

---
+++ 5_Lab/include/queue.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+
+namespace data_structures {
+
+template <class T, class Allocator = std::allocator<T>>
+class queue final {
+
+};
+
+}; // namespace containers
